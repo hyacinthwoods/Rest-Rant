6b47a77842ac5e1de9937096d697a2815fd86952
Hyacinth Woods <Hayispure@gmail.com>
2024-02-17 19:47:46 -0800

    added image

--- views/home.jsx
+++ views/home.jsx
@@ -6,10 +6,16 @@ function home () {
     return (
         <Def>
             <main>
-                <a href="/places">
-  <button className="btn-primary">Places Page</button>
-</a>
                 <h1>HOME</h1>
+                <div>
+                    <img src="/images/hamburger.jpeg"/>
+                    <div>
+                    Photo by <a href="https://unsplash.com/@briewilly?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Chad Montano</a> on <a href="https://unsplash.com/photos/burgers--GFCYhoRe48?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+                    </div>
+                </div>
+                <a href="/places">
+                    <button className="btn-primary">Places Page</button>
+            </a>
             </main>
         </Def>
     )
