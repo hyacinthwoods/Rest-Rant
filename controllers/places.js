a0aa3d8b38edbdf45e2ff0df27c1d9593eea85e6
Hyacinth Woods <Hayispure@gmail.com>
2024-02-20 13:39:17 -0800

    adding and rendering new

--- controllers/places.js
+++ controllers/places.js
@@ -1,17 +1,23 @@
 const router = require('express').Router()
 // GET /places
 
+router.get('/new', (req, res) => {
+    res.render('places/new')
+  })  
+
 router.get('/', (req, res) => {
     let places = [{
         name: 'Cat Cafe',
         city: 'Las Vegas',
         state: 'NV',
         cuisines: 'Coffee, Small Meals',
+        pic: '/images/cafe.jpg'
     }, {
         name: 'Puppy Parlor',
         city: 'Las Vegas',
         state: 'NV',
         cuisines: 'Dog Food',
+        pic: '/images/dog.jpg'
     }]
     res.render('places/index', { places})
 })
--- views/error404.jsx
+++ views/error404.jsx
@@ -5,7 +5,13 @@ function error404 () {
     return (
         <main>
             <h1>404: You Got Lost!!!</h1>
-            <p>Oops! We cant find the page!</p>
+                <p>Oops! We cant find the page!</p>
+                    <div>
+                    <img src= "images/kitten.jpg" alt= "kitten"/>
+                    </div>
+                     <div>
+                     Photo by <a href="https://unsplash.com/@kotecinho?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Kote Puerto</a> on <a href="https://unsplash.com/photos/white-and-gray-kitten-on-white-textile-so5nsYDOdxw?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+                     </div>
         </main>
     )
 }
